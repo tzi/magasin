8c6a0cb1cda3a74b3187437cf74d7378203e7131
Thomas Zilliox <thomas@zilliox.me>
2018-06-05 23:36:43 +0200

    Add mocha

---
+++ test/magasin.spec.js
@@ -0,0 +1,46 @@
+const magasin = require('../src/magasin');
+const assert = require('assert');
+
+describe('Magasin', () => {
+  let store;
+  let listener;
+  let state;
+
+  beforeEach(() => {
+    store = magasin();
+    listener = store.getListener();
+    state = store.addState('character')({
+      firstName: 'FitzChevalerie',
+      lastName: 'Loinvoyant',
+      where: 'Royaume des montagnes'
+    })
+  });
+
+  describe('.onUpdate()', () => {
+    it('should be updated once', () => {
+      let count = 0;
+      listener.onUpdate('character.where', where => {
+        count++;
+        assert.equal(where, 'Castelcerf');
+      });
+      state.update('firstName', 'Fitz');
+      state.update('where', 'Castelcerf');
+
+      assert.equal(count, 1);
+    });
+
+    it('should be updated three times', () => {
+      let count = 0;
+      listener.onUpdate('character.where', () => {
+        count++;
+      });
+      state.update('where', 'Castelcerf');
+      state.update('firstName', 'Fitz');
+      state.update('where', 'Six-Duchés');
+      state.update('where', 'Royaume des montagnes');
+      state.update('firstName', 'Fitz');
+
+      assert.equal(count, 3);
+    });
+  });
+});
